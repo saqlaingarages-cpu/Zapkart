32eab61fad3e087d6c11186d0e2376ff33a81aea
saqlaingarages-cpu <saqlaingarages@gmail.com>
2025-08-02 14:48:10 +0530

    Create Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="text-center p-4 text-sm text-gray-500">
+      © 2025 ZapKart. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
